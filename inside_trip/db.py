4311ab2f7ae5146bda08bdcd2f5e20286e864c89
psw7205 <psw7205@gmail.com>
2019-08-20 10:28:33 +0900

    페이지 추가

--- inside_trip/db.py
+++ inside_trip/db.py
@@ -52,34 +52,220 @@ class MyDB:
             data[col] = pd.to_numeric(data[col])
         data = data.sort_values('소계', ascending=False)
 
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(12, 10))
         return data
 
-    def test1(self):
+    def draw_plot(self):
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+        plot_url = base64.b64encode(img.getvalue()).decode()
+
+        return plot_url
+
+    def popular_rank(self):
         data = self.age_plot_config()
 
-        plt.title('인기많은 관광지순위')
         sns.barplot(x=data.index, y='소계', data=data)
 
-        img = io.BytesIO()
-        plt.savefig(img, format='png')
-        img.seek(0)
-        plot_url = base64.b64encode(img.getvalue()).decode()
+        return self.draw_plot()
 
-        return plot_url
-
-    def test2(self):
+    def popular_age(self):
         data = self.age_plot_config()
 
-        plt.title('20대')
-        sns.violinplot(x='소계', y=data.index, data=data, dodge=False)
+        data = data.iloc[:, 3:]
+        plt.figure(figsize=(16, 9))
+        data.plot(kind='barh', stacked='True')
+        return self.draw_plot()
 
-        img = io.BytesIO()
-        plt.savefig(img, format='png')
-        img.seek(0)
-        plot_url = base64.b64encode(img.getvalue()).decode()
+    def reason(self):
+        plt.rcParams["font.family"] = "Malgun Gothic"
+        plt.rcParams['axes.unicode_minus'] = False
 
-        return plot_url
+        engine = sqlalchemy.create_engine(
+            "mysql+pymysql://test:0000@localhost:3306/inside_trip", encoding='utf8')
 
-    def test3(self):
-        pass
+        conn = engine.connect()
+
+        data_p = pd.read_sql_table('reason', conn)
+        del data_p['index']
+
+        data_p.columns = list(data_p.loc[0])
+        data_p.index = list(data_p['통계분류(2)'])
+
+        data_p = data_p.iloc[4:10, 2:]
+        data_p2 = data_p.T
+
+        for col in data_p2.columns:
+            data_p2[col] = pd.to_numeric(data_p2[col])
+
+        plt.figure(figsize=(16, 10))
+        plt.plot(data_p2, marker='o')
+        plt.xticks(rotation=60)
+
+        return self.draw_plot()
+
+    def load_family_data(self):
+        plt.rcParams["font.family"] = "Malgun Gothic"
+        plt.rcParams['axes.unicode_minus'] = False
+
+        engine = sqlalchemy.create_engine(
+            "mysql+pymysql://test:0000@localhost:3306/inside_trip", encoding='utf8')
+
+        conn = engine.connect()
+
+        return pd.read_sql_table('family_satis', conn)
+
+    def family_income(self):
+        familydata = self.load_family_data()
+
+        f, ax = plt.subplots(2, 3, figsize=(16, 9))
+        ax[0][0].plot(familydata['지역'], familydata['100만~200만원 미만'])
+        ax[0][0].set_title('소득:100만~200만원')
+        ax[0][0].set_ylabel("")
+
+        ax[0][1].plot(familydata['지역'], familydata['200만~300만원 미만'])
+        ax[0][1].set_title('소득:200만~300만원')
+        ax[0][1].set_ylabel("")
+
+        ax[0][2].plot(familydata['지역'], familydata['300만~400만원 미만'])
+        ax[0][2].set_title('소득:300만~400만원')
+        ax[0][2].set_ylabel("")
+
+        ax[1][0].plot(familydata['지역'], familydata['400만~500만원 미만'])
+        ax[1][0].set_title('소득:400만~500만원')
+        ax[1][0].set_ylabel("")
+
+        ax[1][1].plot(familydata['지역'], familydata['500만~600만원 미만'])
+        ax[1][1].set_title('소득:500만~600만원')
+        ax[1][1].set_ylabel("")
+
+        ax[1][2].plot(familydata['지역'], familydata['600만원 이상'])
+        ax[1][2].set_title('소득:600만원~')
+        ax[1][2].set_ylabel("")
+
+        return self.draw_plot()
+
+    def family_population(self):
+        familydata = self.load_family_data()
+
+        plt.subplots(2, 2, figsize=(16, 15))
+
+        plt.subplot(2, 2, 1)
+        ax1 = sns.barplot(familydata['지역'], familydata['2인가구'])
+        ax1.set(ylim=(3.5, 4.5))
+        plt.title('2인가구의 지역별 국내여행 만족도')
+
+        plt.subplot(2, 2, 2)
+        ax2 = sns.barplot(familydata['지역'], familydata['3인가구'])
+        ax2.set(ylim=(3.5, 4.5))
+        plt.title('3인가구의 지역별 국내여행 만족도')
+
+        plt.subplot(2, 2, 3)
+        ax3 = sns.barplot(familydata['지역'], familydata['4인가구'])
+        ax3.set(ylim=(3.5, 4.5))
+        plt.title('4인가구의 지역별 국내여행 만족도')
+
+        plt.subplot(2, 2, 4)
+        ax4 = sns.barplot(familydata['지역'], familydata['5인가구 이상'])
+        ax4.set(ylim=(3.5, 4.5))
+        plt.title('5인가구 이상의 국내여행 만족도')
+
+        return self.draw_plot()
+
+    def family_total(self):
+        familydata = self.load_family_data()
+        plt.subplots()
+        family_citysize = familydata[['지역', '대도시', '중소도시', '읍/면']]
+        family_citysize['종합만족도'] = family_citysize['대도시'] + family_citysize['중소도시'] + family_citysize['읍/면']
+
+        ax = sns.barplot(family_citysize['지역'], family_citysize['종합만족도'])
+        ax.set(ylim=(11.5, 13))
+
+        return self.draw_plot()
+
+    def load_alone_data(self):
+        plt.rcParams["font.family"] = "Malgun Gothic"
+        plt.rcParams['axes.unicode_minus'] = False
+
+        engine = sqlalchemy.create_engine(
+            "mysql+pymysql://test:0000@localhost:3306/inside_trip", encoding='utf8')
+        conn = engine.connect()
+
+        return pd.read_sql_table('alone_satis', conn)
+
+    def alone_male(self):
+        alonedata = self.load_alone_data()
+        alone_sex = alonedata[['지역', '남자', '여자']]
+        alone_sex['여자'] = (alone_sex['여자'] - 3.5)
+        alone_sex['남자'] = (alone_sex['남자'] - 3.5)
+
+        alone_sex['여자'] = alone_sex['여자'] * -1
+        alone_sex = alone_sex.melt('지역', var_name='gender', value_name='vals')
+
+        # draw plot
+        plt.figure(figsize=(13, 10), dpi=80)
+        group_col = 'gender'
+        order_of_bars = alone_sex.지역.unique()[::-1]
+        float(len(alone_sex[group_col].unique()) - 1)
+        colors = [plt.cm.Spectral(i / float(len(alone_sex[group_col].unique()) - 1)) for i in
+                  range(len(alone_sex[group_col].unique()))]
+
+        for c, group in zip(colors, alone_sex[group_col].unique()):
+            sns.barplot(x='vals', y='지역', data=alone_sex.loc[alone_sex[group_col] == group, :], order=order_of_bars,
+                        color=c, label=group)
+
+        # decorations
+        plt.xlabel('만족도')
+        plt.title('남/여 지역별 만족도 차이')
+        plt.legend()
+        return self.draw_plot()
+
+    def alone_age(self):
+        alonedata = self.load_alone_data()
+        alone_age = alonedata[['지역', '15~19세', '20대', '30대', '40대', '50대', '60대이상']]
+        alone_age = alone_age.melt('지역', var_name='age', value_name='val')
+        g = sns.FacetGrid(data=alone_age, hue='age', size=12)
+        g.map(plt.plot, '지역', 'val').add_legend()
+        return self.draw_plot()
+
+    def alone_job(self):
+        alonedata = self.load_alone_data()
+
+        plt.subplots(5, figsize=(16, 15))
+
+        plt.subplot(3, 2, 1)
+        ax1 = sns.barplot(alonedata['지역'], alonedata['전문관리'])
+        ax1.set(ylim=(2.9, 4.5))
+        plt.title('직종:전문관리 국내개인여행 지역별만족도')
+
+        plt.subplot(3, 2, 2)
+        ax2 = sns.barplot(alonedata['지역'], alonedata['사무'])
+        ax2.set(ylim=(2.9, 4.5))
+        plt.title('직종:사무 국내개인여행 지역별만족도')
+
+        plt.subplot(3, 2, 3)
+        ax3 = sns.barplot(alonedata['지역'], alonedata['서비스판매'])
+        ax3.set(ylim=(2.9, 4.5))
+        plt.title('직종:서비스판매 국내개인여행 지역별만족도')
+
+        plt.subplot(3, 2, 4)
+        ax4 = sns.barplot(alonedata['지역'], alonedata['농어업'])
+        ax4.set(ylim=(2.9, 4.5))
+        plt.title('직종:농어업 국내개인여행 지역별만족도')
+
+        plt.subplot(3, 2, 5)
+        ax5 = sns.barplot(alonedata['지역'], alonedata['기능노무'])
+        ax5.set(ylim=(2.9, 4.5))
+        plt.title('직종:기능노무 국내개인여행 지역별만족도')
+        return self.draw_plot()
+
+    def alone_income(self):
+        alonedata = self.load_alone_data()
+        alone_income = alonedata[
+            ['지역', '50만~100만원 미만', '100만~200만원 미만', '200만~300만원 미만', '300만~400만원 미만', '400만~500만원 미만', '500만~600만원 미만',
+             '600만원 이상']]
+        alone_income = alone_income.melt('지역', var_name='income', value_name='val')
+        g = sns.FacetGrid(data=alone_income, hue='income', size=12)
+        g.map(plt.plot, '지역', 'val').add_legend()
+        return self.draw_plot()
--- inside_trip/views.py
+++ inside_trip/views.py
@@ -5,27 +5,30 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from inside_trip import app
-import inside_trip.plot_test as p
 import inside_trip.db as d
-from jinja2 import Markup
 
 year = datetime.now().year
+database = d.MyDB()
 
+satis_img1 = database.family_income()
+satis_img2 = database.family_population()
+satis_img3 = database.family_total()
+
+age_img1=database.popular_rank()
+age_img2=database.popular_age()
+age_img3=database.reason()
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    folium_html_tag = Markup(p.folium_test())
-    temp = d.MyDB()
 
     return render_template(
         'index.html',
         title='INSIDE TRIP',
         year=year,
-        img=temp.test1(),
-        img2=temp.test2(),
-        map=folium_html_tag
+        img=database.popular_rank(),
+        img2=database.popular_age()
     )
 
 
@@ -48,30 +51,43 @@ def about():
     )
 
 
-@app.route('/home/test')
-def test():
-    return render_template(
-        'detail.html',
-        img=p.test1(),
-        img2=p.test3(),
-        title='TEST',
-        year=year
-    )
-
-
 @app.route('/home/age')
 def age():
     return render_template(
-        'detail.html',
-        title='나이별 추천 여행지',
+        'age.html',
+        title='연령대 별 여행지 선호도 조사',
+        img1=age_img1,
+        img2=age_img2,
+        img3=age_img3,
         year=year
     )
 
 
-@app.route('/home/cost')
-def cost():
+@app.route('/home/family')
+def family():
     return render_template(
-        'detail.html',
-        title='가성비 추천 정보',
-        year=year
+        'family.html',
+        title='여행 만족도 분석',
+        year=year,
+        img1=satis_img1,
+        img2=satis_img2,
+        img3=satis_img3
     )
+
+@app.route('/home/alone')
+def alone():
+    return render_template(
+        'alone.html',
+        title='여행 만족도 분석',
+        year=year,
+        img1=alone_img1,
+        img2=alone_img2,
+        img3=alone_img3,
+        img4=alone_img4
+    )
+
+
+alone_img1 = database.alone_male()
+alone_img2 = database.alone_age()
+alone_img3 = database.alone_job()
+alone_img4 = database.alone_income()
